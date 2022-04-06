8b31a0376e28f035c07b76c689cefb4441a752ad
F0716627 Amon Gabriel dos Santos Paiva <amon_gabriel@bb.com.br>
2022-04-05 23:48:19 -0300

    configuração da porta do swagger

--- src/config/customExpress.js
+++ src/config/customExpress.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
+const SWEGGER_PORT = process.env.SWEGGER_PORT || PORT;
 const URL = process.env.URL || 'localhost';
 
 let options = {
@@ -10,7 +11,7 @@ let options = {
       title: 'MercadoBot - API base',
       version: '1.0.0',
     },
-    host: URL + ':' + PORT,
+    host: URL + ':' + SWEGGER_PORT,
     basePath: '/',
     produces: [
       "application/json"
