f6bb5673b14f2ef9b6ded1ae2b04b236828bf468
F0716627 Amon Gabriel dos Santos Paiva <amon_gabriel@bb.com.br>
2022-04-07 23:58:09 -0300

    configure production dabatabase connection

--- src/config/knexfile.js
+++ src/config/knexfile.js
@@ -15,63 +15,10 @@ module.exports = {
     }
   },
   production: {
-    client: 'mysql',
+    client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_SCHEMA
-    },
-    pool: {
-      min: 2,
-      max: 10
+      connectionString: process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
     }
   }
-
-};
-
-// module.exports = {
-//   staging: {
-//     client: 'postgresql',
-//     connection: {
-//       database: 'my_db',
-//       user: 'username',
-//       password: 'password'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     },
-//     migrations: {
-//       tableName: 'knex_migrations'
-//     }
-//   },
-//   production: {
-//     client: 'postgres',
-//     connection: {
-//       host: 'ec2-54-173-77-184.compute-1.amazonaws.com',
-//       port: 5432,
-//       user: 'vsqnofpecfpedb',
-//       password: '9af9d7a6a7841d914ac3a16ffda0d4b862b71f552d79c687a6f5a236faff2640',
-//       database: 'dago3spqs1cs5a'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     }
-//   },
-//   development: {
-//     client: 'mysql',
-//     connection: {
-//       host: '127.0.0.1',
-//       user: 'root',
-//       password: 'amon1513',
-//       database: 'meiabarra'
-//     },
-//     pool: {
-//       min: 2,
-//       max: 10
-//     }
-//   }
-
-// };
+}
